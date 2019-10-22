6133a30f6bae15c352e2c6f7bc8f7bf167f924be
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-10-22 16:38:41 +0530

    Added unstyled color slider

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import './App.css';
 class App extends Component {
 
   render() {
-    console.log(generatePalette(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import './Palette.css';
 import ColorBox from './ColorBox';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500 };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+
+    changeLevel(newLevel) {
+        this.setState({ level: newLevel })
+    }
+
     render() {
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
         ));
 
         return (
             <div className="Palette">
+                <Slider
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                />
+
                 {/* Navbar goes here */}
                 <div className="Palette-colors">
                     {colorBoxes}
